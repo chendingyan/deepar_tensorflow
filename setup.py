464a0df9d93cbcf4d4d1a7aeb5f06fdb80bc7064
jgleason <jeffrey.gleason@newknowledge.io>
2019-11-19 11:57:14 -0600

    bump dependencies to be in line with d3m core

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='deepar',
       author='Jeffrey Gleason',
       author_email='jeffrey.gleason@yonder.co',
       url='https://github.com/NewKnowledge/deepar/tree/master',
-      install_requires=['tensorflow-gpu==2.0.0', 'numpy', 'pandas', 'sklearn'],
+      install_requires=['tensorflow==2.0.0', 'numpy', 'pandas', 'sklearn'],
       packages=find_packages()
      )
